f9218f4751e25fb0417eee426ca78af856076998
Fang Zhiyong <fzhiyong@datto.com>
2024-08-06 18:52:08 +0200

    mix k6 runner

--- browserTest.js
+++ browserTest.js
@@ -2,41 +2,52 @@
     import { check } from 'k6';
     import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/2.4.0/dist/bundle.js";
    
-   
-    export let options = {
+    export const options = {
+        scenarios: {
+          ui: {
+            executor: 'shared-iterations',
+            vus: 5,
+            iterations: 10,
+            options: {
+              browser: {
+                type: 'chromium',
+              },
+            },
+          },
+        },
+      }
+    export let optionsx = {
         vus: 5,
         iterations: 10
 
     }
 
     export default async function () {
-        //const browser = chromium.launch({ headless: false });
+        const page = browser.newPage();
+      
+        try {
+            await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
+            page.screenshot({ path: 'screenshots/browserTestScreenshot.png' });
+   
+           page.locator('#input-email').type('lambdatest.Cypress@disposable.com');
+           page.locator('#input-password').type('Cypress123!!');
+           const submitButton = page.locator('input[value="Login"]');
+           await Promise.all([page.waitForNavigation(), submitButton.click()]);
+   
+           check(page, {
+               'Verify user is logged In': () =>
+                   page.locator('.breadcrumb-item.active').textContent() == 'Account',
+           });
+           check(page, {
+               'Verify the text': () =>
+                   page.locator('.breadcrumb-item.active').textContent() == 'Test',
+           });
+        } finally {
+          page.close();
+        }
+      }    
 
-        const browser = chromium.launch({ args: ['no-sandbox'], headless: true, timeout: '60s' });
-        const context = browser.newContext();
-        const page = context.newPage();
-
-        await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
-         page.screenshot({ path: 'screenshots/browserTestScreenshot.png' });
-
-        page.locator('#input-email').type('lambdatest.Cypress@disposable.com');
-        page.locator('#input-password').type('Cypress123!!');
-        const submitButton = page.locator('input[value="Login"]');
-        await Promise.all([page.waitForNavigation(), submitButton.click()]);
-
-        check(page, {
-            'Verify user is logged In': () =>
-                page.locator('.breadcrumb-item.active').textContent() == 'Account',
-        });
-        check(page, {
-            'Verify the text': () =>
-                page.locator('.breadcrumb-item.active').textContent() == 'Test',
-        });
-
-
-         page.close()
-         browser.close();
-    }
+    
 
     export function handleSummary(data) {
         return {
